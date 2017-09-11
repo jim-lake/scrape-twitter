3745a1be9cd060398a69d0917cea8d5ab2efb68a
Jim Lake <jim@blueskylabs.com>
2017-09-10 18:36:47 -0700

    need dist to be able to use this as a module from github

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var TimelineStream = require('./lib/timeline-stream');
+var ConversationStream = require('./lib/conversation-stream');
+var ThreadedConversationStream = require('./lib/threaded-conversation-stream');
+var TweetStream = require('./lib/tweet-stream');
+var ListStream = require('./lib/list-stream');
+var LikeStream = require('./lib/like-stream');
+var ConnectionStream = require('./lib/connection-stream');
+var getUserProfile = require('./lib/twitter-query').getUserProfile;
+
+module.exports = {
+  TimelineStream: TimelineStream,
+  ConversationStream: ConversationStream,
+  ThreadedConversationStream: ThreadedConversationStream,
+  TweetStream: TweetStream,
+  ListStream: ListStream,
+  LikeStream: LikeStream,
+  ConnectionStream: ConnectionStream,
+  getUserProfile: getUserProfile
+};
